667737251a1425235b24ace8397014d907c2ec74
WFluid <88296160+Wfluid@users.noreply.github.com>
2021-12-24 15:06:02 +0800

    添加触发模块

--- main.ts
+++ main.ts
@@ -6,6 +6,22 @@ enum TalkerPin {
     P15 = 115
 }
 
+enum TalkerWords {
+    //% block=开机
+    wakeup = 0,
+    //% block=开灯
+    poweron,
+    //% block=关灯
+    poweroff,
+    //% block=发送
+    send,
+    //% block=你好
+    hello,
+    //% block=再见
+    goodbye,
+}
+const TalkerWordsStr = ['wakeup', 'poweron', 'poweroff', 'send', 'hello', 'bye'];
+
 /**
  * Talking extension for SU-03T
  */
@@ -17,6 +33,20 @@ namespace CoolTalker {
     //% block="初始化小酷宝在 %pin| " weight=100
     export function TalkerInit(pin: TalkerPin) {
         serial.redirect(pin + 1, pin + 0, 9600);
+    }
+
+    //% blockId=CoolTalker_when_receive
+    //% block="当听见| %word| " weight=90
+    export function WhenReceiveWord(word: string, body: () => void) {
+        let talkres = '';
+        serial.onDataReceived(serial.delimiters(Delimiters.NewLine), () => {
+            talkres = serial.readString();
+        });
+        basic.showString(talkres);
+        basic.showString(word);
+        if (talkres == word) {
+            body();
+        }
     }
 
 }
